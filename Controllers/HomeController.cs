eb95343fedcec52612d91b16e680279f6bb94301
Roman Matviienko <rmatviienko@softjournua.onmicrosoft.com>
2021-07-02 10:02:25 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ValidationApp.Models;
+
+namespace ValidationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Redirect("~/Home/Create");
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Person person)
+        {
+            if (ModelState.IsValid)
+                return Content($"{person.Name} - {person.Email}");
+            else
+                return View(person);
+        }
+    }
+}
